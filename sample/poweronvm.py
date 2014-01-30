d43936417e6f3fb6d61eb0dbc74a0c88beea5060
Philippe Dellaert <philippe@dellaert.org>
2014-01-30 21:41:19 +0100

    Alphabetical order

--- sample/poweronvm.py
+++ sample/poweronvm.py
@@ -36,8 +36,8 @@ def GetArgs():
    parser.add_argument('-s', '--host', required=True, action='store', help='Remote host to connect to')
    parser.add_argument('-o', '--port', type=int, default=443, action='store', help='Port to connect on')
    parser.add_argument('-u', '--user', required=True, action='store', help='User name to use when connecting to host')
-   parser.add_argument('-v', '--vmname', required=True, action='append', help='Names of the Virtual Machines to power on')
    parser.add_argument('-p', '--password', required=False, action='store', help='Password to use when connecting to host')
+   parser.add_argument('-v', '--vmname', required=True, action='append', help='Names of the Virtual Machines to power on')
    args = parser.parse_args()
    return args
 
