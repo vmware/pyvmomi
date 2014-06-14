9351a88e81a8d50a79b36a8da2bf808a8c7b7553
Michael Rice <michael.rice@rackspace.com>
2014-06-14 16:02:43 -0500

    reformat cache.py from 3 spaces to 4

--- pyVmomi/Cache.py
+++ pyVmomi/Cache.py
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,25 +18,27 @@ This module implements the cache decorator
 """
 __author__ = "VMware, Inc."
 
+
 def Cache(fn):
-   """ Function cache decorator """
-   def fnCache(*args, **kwargs):
-      """ Cache function """
-      key = (args and tuple(args) or None,
-             kwargs and frozenset(kwargs.items()) or None)
-      if key not in fn.__cached__:
-         fn.__cached__[key] = cache = fn(*args, **kwargs)
-      else:
-         cache = fn.__cached__[key]
-      return cache
+    """ Function cache decorator """
 
-   def ResetCache():
-      """ Reset cache """
-      fn.__cached__ = {}
+    def fnCache(*args, **kwargs):
+        """ Cache function """
+        key = (args and tuple(args) or None,
+               kwargs and frozenset(kwargs.items()) or None)
+        if key not in fn.__cached__:
+            fn.__cached__[key] = cache = fn(*args, **kwargs)
+        else:
+            cache = fn.__cached__[key]
+        return cache
 
-   setattr(fn, "__cached__", {})
-   setattr(fn, "__resetcache__", ResetCache)
-   fnCache.__name__ = fn.__name__
-   fnCache.__doc__ = fn.__doc__
-   fnCache.__dict__.update(fn.__dict__)
-   return fnCache
+    def ResetCache():
+        """ Reset cache """
+        fn.__cached__ = {}
+
+    setattr(fn, "__cached__", {})
+    setattr(fn, "__resetcache__", ResetCache)
+    fnCache.__name__ = fn.__name__
+    fnCache.__doc__ = fn.__doc__
+    fnCache.__dict__.update(fn.__dict__)
+    return fnCache
