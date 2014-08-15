fdc2d28bc02b62d3f8dfe4886da0a2883da9f9e2
Shawn Hartsock <hartsocks@vmware.com>
2014-08-15 17:02:25 -0400

    Validate 'Iso8601' is properly defined.
    Deliberately exercises the code on 313 of VmomiSupport.py
    to validate that the import for datetime parsing is working
    as expected.
    
    closes: https://github.com/vmware/pyvmomi/issues/112

---
+++ tests/test_iso8601.py
@@ -0,0 +1,44 @@
+# VMware vSphere Python SDK
+# Copyright (c) 2008-2014 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import atexit
+from datetime import datetime
+
+from tests import fixtures_path
+import unittest
+import vcr
+
+from pyVim import connect
+
+class Iso8601Tests(unittest.TestCase):
+
+    @vcr.use_cassette('test_vm_config_iso8601.yaml',
+                      cassette_library_dir=fixtures_path, record_mode='once')
+    def test_vm_config_iso8601(self):
+        si = connect.SmartConnect(host='vcsa',
+                                  user='my_user',
+                                  pwd='my_password')
+        atexit.register(connect.Disconnect, si)
+
+        search_index = si.content.searchIndex
+        uuid = "5001ad1b-c78d-179e-ecd7-1cc0e1cf1b96"
+        vm = search_index.FindByUuid(None, uuid, True, True)
+        boot_time = vm.runtime.bootTime
+        # NOTE (hartsock): assertIsNone does not work in Python 2.6
+        self.assertTrue(boot_time is not None)
+
+        # 2014-08-05T17:50:20.594958Z
+        expected_time = datetime(2014, 8, 5, 17, 50, 20, 594958,
+                                 boot_time.tzinfo)
+        self.assertEqual(expected_time, boot_time)
