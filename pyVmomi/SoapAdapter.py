a5a3aa996eb0c46824073d72716c0116e1235944
Arnon Yaari <arnony@infinidat.com>
2014-09-14 17:36:07 +0300

    Add SoapAdapter request context per instance.
    SoapStubAdapter supports adding soap headers using GetRequestContext, but
    the request context is thread-local. Adding support for a per-instance request context.
    This is needed e.g. for vSphere SMS API, where only the SMS SoapAdapter needs
    the additional headers.

--- pyVmomi/SoapAdapter.py
+++ pyVmomi/SoapAdapter.py
@@ -901,6 +901,8 @@ class SoapStubAdapterBase(StubAdapterBase):
 
       # Add request context and samlToken to soap header, if exists
       reqContexts = GetRequestContext()
+      if self.requestContext:
+         reqContexts.update(self.requestContext)
       samlToken = getattr(self, 'samlToken', None)
 
       if reqContexts or samlToken:
@@ -1197,7 +1199,7 @@ class SoapStubAdapter(SoapStubAdapterBase):
                 thumbprint=None, cacertsFile=None, version=None,
                 acceptCompressedResponses=True,
                 connectionPoolTimeout=CONNECTION_POOL_IDLE_TIMEOUT_SEC,
-                samlToken=None, sslContext=None):
+                samlToken=None, sslContext=None, requestContext=None):
       if ns:
          assert(version is None)
          version = versionMap[ns]
@@ -1263,6 +1265,7 @@ class SoapStubAdapter(SoapStubAdapterBase):
       if sslContext:
          self.schemeArgs['context'] = sslContext
       self.samlToken = samlToken
+      self.requestContext = requestContext
       self.requestModifierList = []
       self._acceptCompressedResponses = acceptCompressedResponses
 
