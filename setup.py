30b3c9a4d17a163da160919a746a9c5d1cedc1de
het <het@vmware.com>
2018-09-05 21:22:57 -0700

    Update version for 6.7.0 patch release

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open('test-requirements.txt') as f:
 
 setup(
    name='pyvmomi',
-   version='6.7.0',
+   version='6.7.0.2018.9',
    description='VMware vSphere Python SDK',
    # NOTE: pypi prefers the use of RST to render docs
    long_description=read('README.rst'),
