97d91fe194778e9f6da131d50402e3706708a9e5
Pavan Bidkar <pbidkar@vmware.com>
2019-09-05 17:05:56 +0530

    Changes to update new vSphere release 67U3  (#835)
    * Resolved conflicts
    
    * Updated README as per review comments.
    
    * Updated README in Python Support section
    
    * Updated README as per review comments.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # VMware vSphere Python SDK
-# Copyright (c) 2009-2018 VMware, Inc. All Rights Reserved.
+# Copyright (c) 2009-2019 VMware, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ with open('test-requirements.txt') as f:
 
 setup(
    name='pyvmomi',
-   version='6.7.1.2018.12',
+   version='6.7.3',
    description='VMware vSphere Python SDK',
    # NOTE: pypi prefers the use of RST to render docs
    long_description=read('README.rst'),
@@ -53,6 +53,7 @@ setup(
       'Programming Language :: Python :: 3.4',
       'Programming Language :: Python :: 3.5',
       'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
       'Topic :: Software Development :: Libraries :: Python Modules',
       'Topic :: System :: Distributed Computing',
       'Operating System :: Microsoft :: Windows',
