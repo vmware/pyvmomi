87d4cb3ab0f0b7237172823bf2bbb65ccac5ffde
ddraganov <ddraganov@vmware.com>
2023-01-18 17:13:13 +0200

    [Emulate legacy behavior] Degrade types unsupported on the wire to string

--- pyVmomi/StubAdapterAccessorImpl.py
+++ pyVmomi/StubAdapterAccessorImpl.py
@@ -1,8 +1,20 @@
 # **********************************************************
-# Copyright (c) 2011-2022 VMware, Inc.
+# Copyright (c) 2011-2023 VMware, Inc.
 # **********************************************************
 
 from .VmomiSupport import GetVmodlType, ManagedObject, Object
+
+
+# Used to replace few type definitions for emulating
+# legacy behavior. Ref. VmomiSupport._wsdlTypeMap
+_legacyTypes = {
+    "type": "string",
+    "ManagedMethod": "string",
+    "PropertyPath": "string",
+    "type[]": "string[]",
+    "ManagedMethod[]": "string[]",
+    "PropertyPath[]": "string[]"
+}
 
 
 class StubAdapterAccessorMixin:
@@ -15,6 +27,12 @@ class StubAdapterAccessorMixin:
     def InvokeAccessor(self, mo, info):
         prop = info.name
         param = Object(name="prop", type=str, version=self.version, flags=0)
+
+        # Emulate legacy behavior for backward compatibility
+        replacement = _legacyTypes.get(info.type.__name__, None)
+        if replacement:
+            info.type = GetVmodlType(replacement)
+
         info = Object(name=info.name,
                       type=ManagedObject,
                       wsdlName="Fetch",
