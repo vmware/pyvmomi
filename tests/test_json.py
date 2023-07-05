e2ab1a71f1c0595bd164f760c04bf68d7f0cb0d0
ddraganov <ddraganov@vmware.com>
2023-07-06 01:48:37 +0300

    [tests] Enable all VmomiJSONEncoder tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -19,10 +19,8 @@ import tests
 
 from pyVim import connect
 from pyVmomi import vim
-from pyVmomi.VmomiSupport import VmomiJSONEncoder, templateOf
-from six import PY3
+from pyVmomi.VmomiJSONEncoder import VmomiJSONEncoder, templateOf
 
-import unittest
 import atexit
 import inspect
 import json
@@ -84,7 +82,6 @@ class JSONTests(tests.VCRTestBase):
 
     # Explodes the VM
     # By definition if the input is a ManagedObject then it explodes.
-    @unittest.skip("skipped for 7.0 release")
     @tests.VCRTestBase.my_vcr.use_cassette(
         'test_json_vm_explode_default.yaml',
         cassette_library_dir=tests.fixtures_path, record_mode='once')
@@ -103,7 +100,6 @@ class JSONTests(tests.VCRTestBase):
 
     # Explodes the VM disabling the dynamic field stripping
     # By definition if the input is a ManagedObject then it explodes.
-    @unittest.skip("skipped for 7.0 release")
     @tests.VCRTestBase.my_vcr.use_cassette(
         'test_json_vm_explode_strip_dynamic.yaml',
         cassette_library_dir=tests.fixtures_path, record_mode='once')
@@ -123,7 +119,6 @@ class JSONTests(tests.VCRTestBase):
 
     # Explodes the VM and the VM's networks
     # Here self.vm is redundant (see above) but not harmful.
-    @unittest.skip("skipped for 7.0 release")
     @tests.VCRTestBase.my_vcr.use_cassette(
         'test_json_vm_explode_objs.yaml',
         cassette_library_dir=tests.fixtures_path, record_mode='once')
@@ -135,7 +130,6 @@ class JSONTests(tests.VCRTestBase):
                                explode=to_explode))
 
     # Explodes by type: all VirtualMachine and all of its snapshots
-    @unittest.skip("skipped for 7.0 release")
     @tests.VCRTestBase.my_vcr.use_cassette(
         'test_json_vm_explode_type.yaml',
         cassette_library_dir=tests.fixtures_path, record_mode='once')
@@ -146,7 +140,6 @@ class JSONTests(tests.VCRTestBase):
                                         templateOf('VirtualMachineSnapshot')]))
 
     # Test Datacenter
-    @unittest.skip("skipped for 7.0 release")
     @tests.VCRTestBase.my_vcr.use_cassette(
         'test_json_datacenter_explode.yaml',
         cassette_library_dir=tests.fixtures_path, record_mode='once')
@@ -155,7 +148,6 @@ class JSONTests(tests.VCRTestBase):
                                  sort_keys=True))
 
     # Test Datastore
-    @unittest.skip("skipped for 7.0 release")
     @tests.VCRTestBase.my_vcr.use_cassette(
         'test_json_datastore_explode.yaml',
         cassette_library_dir=tests.fixtures_path, record_mode='once')
@@ -164,7 +156,6 @@ class JSONTests(tests.VCRTestBase):
                                  sort_keys=True))
 
     # Test HostSystem
-    @unittest.skip("skipped for 7.0 release")
     @tests.VCRTestBase.my_vcr.use_cassette(
         'test_json_host_explode.yaml',
         cassette_library_dir=tests.fixtures_path, record_mode='once')
@@ -173,7 +164,6 @@ class JSONTests(tests.VCRTestBase):
                                  sort_keys=True))
 
     # Test Network
-    @unittest.skip("skipped for 7.0 release")
     @tests.VCRTestBase.my_vcr.use_cassette(
         'test_json_network_explode.yaml',
         cassette_library_dir=tests.fixtures_path, record_mode='once')
